3df2f52c607dc9ac67b141edf4398e974f2b5c89
mtj-98 <129771239+mtj-98@users.noreply.github.com>
2023-06-27 22:24:06 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,165 @@
+// -------- #1
+
+// const productName = "Droid";
+
+// const pricePerItem = 2000;
+
+// console.log (productName);
+// console.log(pricePerItem);
+
+
+
+
+
+// --------- #2
+// let pricePerItem = 2000;
+// pricePerItem = 3500;
+
+// let productName = "Droid";
+// productName = "Repair droid";
+
+// console.log(pricePerItem);
+// console.log (productName);
+
+
+// -------- #3
+// const topSpeed = 160;
+
+// let distance = 617.54;
+
+// const login = "mango935";
+
+// const isOnline = true;
+
+// const isAdmin = false;
+
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// colsole.log(isOnline);
+// console.log(isAdmin);
+
+
+// -------- #4
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = (pricePerItem * orderedQuantity);
+
+// console.log(totalPrice);
+
+
+// ------- #5
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+
+// console.log(message);
+
+
+// -------- #6
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// console.log(totalPrice);
+
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// ------ #7
+// function sayHi() {
+//     console.log('Hello, this is my first function!');
+// }
+
+// sayHi();
+
+// ------- #8
+
+// function add(a, b, c) {
+//     console.log(`Addition result equals ${a + b + c}`);
+//   }
+
+//   add(15, 27, 10);
+//   add(10, 20, 30);
+//   add(5, 10, 15);
+
+// ------ #9
+// function add(a, b, c) {
+  
+//     return a + b + c;
+//   }
+  
+//   add(2, 5, 8); // 15
+  
+//   console.log(add(15, 27, 10));
+//   console.log(add(10, 20, 30));
+//   console.log(add(5, 10, 15));
+
+// ------ #10
+// function makeMessage (name, price) {
+//     // Change code below this line
+//      const message = `You picked ${name}, price per item is ${price} credits`;
+//     // Change code above this line
+//     return message;
+//   };
+  
+//   console.log(makeMessage('Radar', 6150));
+//   console.log(makeMessage('Scanner', 3500));
+//   console.log(makeMessage('Reactor', 8000));
+//   console.log(makeMessage('Engine', 4070));
+
+
+// ------ #11
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//     // Change code below this line
+//     const totalPrice = (orderedQuantity * pricePerItem);
+  
+//     // Change code above this line
+//     return totalPrice;
+//   };
+
+
+// console.log(calculateTotalPrice(5, 100));
+// console.log(calculateTotalPrice(8, 60));
+// console.log(calculateTotalPrice(3, 400));
+// console.log(calculateTotalPrice(1, 3500));
+// console.log(calculateTotalPrice(12, 70));
+
+
+// -------- #12
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//     // Change code below this line
+
+//     const totalPrice = (orderedQuantity * pricePerDroid + deliveryFee);
+  
+//     const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+  
+//     // Change code above this line
+//     return message;
+//   }
+
+//   console.log(makeOrderMessage(2, 100, 50));
+//   console.log(makeOrderMessage(4, 300, 100));
+//   console.log(makeOrderMessage(10, 70, 200));
+
+
+// -------- #13
+function isAdult(age) {
+    // Change code below this line
+    const passed = 18;
+  
+    // Change code above this line
+    return passed;
+  }
+  
+  console.log(isAdult (passed) >= 20);
+
+
+// const x = 5;
+// const y = 10;
+// const z = 5;
+
+// console.log(x > y);
